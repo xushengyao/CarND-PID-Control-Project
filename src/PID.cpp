54776d490df0cfb1e564c9e6732b7cc7ac464fdb
Kite's Runner <shengyao@umich.edu>
2018-03-15 23:30:02 -0400

    initial_test

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+  int_cte = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+  this->d_error = cte - this->p_error;
+  this->p_error = cte;
+  this->i_error += cte;
 }
 
 double PID::TotalError() {
+  return -this->Kp * this->p_error - this->Kd * this->d_error - this->Ki * this->i_error;
 }
-
